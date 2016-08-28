3ea24a77bb626f6ecc806029d41bad22e38ca122
davidvypark <davidvypark@gmail.com>
2016-08-28 16:36:30 -0400

    added UIElements to custom quotetableviewcell

--- Quotes/QuoteTableViewCell.swift
+++ Quotes/QuoteTableViewCell.swift
@@ -6,8 +6,40 @@
 //  Copyright © 2016 DavidVY. All rights reserved.
 //
 
-import Cocoa
+import UIKit
+import SnapKit
 
 class QuoteTableViewCell: UITableViewCell {
+	
+	let profilePic = UIImageView()
+	let usernameLabel = UILabel()
+	let quoteBoxLabel = UILabel()
+	let dateLabel = UILabel()
+	let heardByLabel = UILabel()
+	let heardPersonLabel = UILabel()
+	
+	
+	let cellIdentifier = "QuoteTablewViewCell"
+	
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		
+		setupCell()
+	}
+	
+	func setupCell() {
+		
+		addSubview(quoteBoxLabel)
+		
+		addSubview(heardByLabel)
+		
+		addSubview(heardPersonLabel)
+		
+		addSubview(profilePic)
+		
+		addSubview(usernameLabel)
+		
+		addSubview(dateLabel)
+	}
 
 }
