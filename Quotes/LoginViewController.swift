6e028acca5a9bf34fcc244d51f3090a70692aa79
davidvypark <davidvypark@gmail.com>
2016-09-05 21:53:35 -0400

    implemented phone number formatting in loginVC

--- Quotes/LoginViewController.swift
+++ Quotes/LoginViewController.swift
@@ -50,14 +50,48 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
 	
 	func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
 		
-		if(textField == phoneNumberTextField) {
-			if(textField.text!.characters.count - range.length + string.characters.count > 10) {
-				return false
+		
+		if (textField == phoneNumberTextField || textField == loginWithPhoneNumberTextField) {
+			let newString = (textField.text! as NSString).stringByReplacingCharactersInRange(range, withString: string)
+			let components = newString.componentsSeparatedByCharactersInSet(NSCharacterSet.decimalDigitCharacterSet().invertedSet)
+			
+			let decimalString = components.joinWithSeparator("") as NSString
+			let length = decimalString.length
+			let hasLeadingOne = length > 0 && decimalString.characterAtIndex(0) == (1 as unichar)
+			
+			if length == 0 || (length > 10 && !hasLeadingOne) || length > 11
+			{
+				let newLength = (textField.text! as NSString).length + (string as NSString).length - range.length as Int
+				
+				return (newLength > 10) ? false : true
 			}
-		} else if(textField == loginWithPhoneNumberTextField) {
-			if(textField.text!.characters.count - range.length + string.characters.count > 10) {
-				return false
+			var index = 0 as Int
+			let formattedString = NSMutableString()
+			
+			if hasLeadingOne
+			{
+				formattedString.appendString("1 ")
+				index += 1
 			}
+			if (length - index) > 3
+			{
+				let areaCode = decimalString.substringWithRange(NSMakeRange(index, 3))
+				formattedString.appendFormat("(%@)", areaCode)
+				index += 3
+			}
+			if length - index > 3
+			{
+				let prefix = decimalString.substringWithRange(NSMakeRange(index, 3))
+				formattedString.appendFormat("%@-", prefix)
+				index += 3
+			}
+			
+			let remainder = decimalString.substringFromIndex(index)
+			formattedString.appendString(remainder)
+			textField.text = formattedString as String
+			return false
+
+
 		}
 		return true
 	}
